7949f4abcd4800277b6a2f88a09d06a91929adb6
MrGaoGang <gaogangwork@qq.com>
2018-05-24 23:50:32 +0800

    完成主界面和选择类别界面，使用Rxbus

--- app/src/main/java/com/mrwho/kotlindemo/activity/MainActivity.kt
+++ app/src/main/java/com/mrwho/kotlindemo/activity/MainActivity.kt
@@ -1,46 +1,60 @@
 package com.mrwho.kotlindemo.activity
 
+import android.content.Intent
 import android.graphics.Typeface
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.content.ContextCompat
 import android.util.TypedValue
+import android.view.View
 import android.widget.TextView
+import com.chen.kotlintext.utils.SPUtils
 import com.mrwho.kotlindemo.R
 import com.mrwho.kotlindemo.adapter.MainFragmentAdapter
 import com.mrwho.kotlindemo.base.BaseActivity
+import com.mrwho.kotlindemo.beans.SelectChange
 import com.mrwho.kotlindemo.utils.MainDataProvider
+import com.mrwho.kotlindemo.utils.MainItem
+import com.mrwho.kotlindemo.utils.RxBus
 import com.mrwho.kotlindemo.utils.StatusBarUtils
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), View.OnClickListener {
 
-
+    private var nowPosition = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         StatusBarUtils.transparencyBar(this)
-        initView()
+
+        addTabImg.setOnClickListener(this)
+        initBus()
+        initViewPager()
     }
 
 
-    fun initView() {
-        val list = MainDataProvider.getItems()
+    fun initViewPager() {
 
-        viewPager.adapter = MainFragmentAdapter(supportFragmentManager, list)
-        viewPager.currentItem = 0
+        viewPager.adapter = MainFragmentAdapter(supportFragmentManager, getSelectList())
+
+        if (viewPager.adapter.count > nowPosition) {
+            viewPager.currentItem = nowPosition
+        } else {
+            viewPager.currentItem = viewPager.adapter.count - 1
+        }
 
         tabLayout.tabMode = TabLayout.MODE_SCROLLABLE
         tabLayout.setupWithViewPager(viewPager)
-
 
         for (each in 0..viewPager.adapter.count - 1) {
             val tab = tabLayout.getTabAt(each)
             tab?.setCustomView(R.layout.main_item_tab)
             val textView = tab?.customView?.findViewById(R.id.tab_item) as TextView
-            textView.setText(viewPager.adapter.getPageTitle(each))
+            textView.text = viewPager.adapter.getPageTitle(each)
 
-            if (each == 0) {
+            if (each == nowPosition) {
                 updateText(tab, true)
             }
 
@@ -61,11 +75,32 @@ class MainActivity : BaseActivity() {
         })
     }
 
+    private fun initBus() {
+        RxBus.instance.observe(SelectChange::class.java)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    initViewPager()
+                }
+
+    }
+
+    /**
+     * 获取到选择显示的栏目
+     */
+    private fun getSelectList(): ArrayList<MainItem> {
+        val list = MainDataProvider.getItems()
+        val selectList = ArrayList<MainItem>()
+        list.filterTo(selectList) { SPUtils.getSelect(this, it.id) }
+
+        return selectList
+    }
 
     /**
      * 如果选中了的
      */
     private fun updateText(tab: TabLayout.Tab, select: Boolean) {
+        nowPosition = tab.position
         val textView = tab.customView?.findViewById(R.id.tab_item) as TextView
         if (select) {
             textView.setTextColor(ContextCompat.getColor(this, R.color.white))
@@ -80,6 +115,15 @@ class MainActivity : BaseActivity() {
 
         }
 
+    }
+
+    /**
+     * 点击事件
+     */
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.addTabImg -> startActivity(Intent(this, AddTabsActivity::class.java))
+        }
     }
 
 }
---
+++ app/src/main/java/com/mrwho/kotlindemo/beans/SelectChange.kt
@@ -0,0 +1,12 @@
+package com.mrwho.kotlindemo.beans
+
+/**
+ * Created by mr.gao on 2018/5/24.
+ * Package:    com.mrwho.kotlindemo.beans
+ * Create Date:2018/5/24
+ * Project Name:KotlinDemo
+ * Description:
+ */
+class SelectChange() {
+
+}
